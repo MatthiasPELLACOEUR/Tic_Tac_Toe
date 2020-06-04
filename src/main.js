f3ef3b42597b5af71c60664e045971bbe64b5277
MatthiasPELLACOEUR <matthias.pellacoeur@hotmail.com>
2020-06-04 02:28:32 +0200

    tryed some things for nowin result

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-class Game
-{
-  constructor(){
+class Game {
+  constructor() {
     // this.started = false
     this.turn = true
     // this.launchGame = document.getElementById('launchGame')
     // this.playerX = "X"
     // this.playerO = "O"
     this.squares = document.getElementsByClassName('square')
+    this.reset = document.getElementById('reset')
   }
-  
+
   // launchGame(){
-    // this.launchGame.addEventListener('click', function(e){
+  // this.launchGame.addEventListener('click', function(e){
   // When click on start button associate cross or circle on player1 and start timer
-      // if(document.getElementById('cross').checked == true){
+  // if(document.getElementById('cross').checked == true){
   //       this.assignPlayer("X", "O")
   //     }
   //     else if(document.getElementById('circle').checked == true){
@@ -30,93 +30,118 @@ class Game
   //   player2 = new Player(checker2, false, 0)
   // }
 
-  win()
-  {
+  
+  resetGame(){
+    console.log('test');
+      // document.location.reload(true);
+      
+  }
+
+  win() {
     let tableau = [
-      ["00","01","02"],
-      ["10","11","12"],
-      ["20","21","22"],
-      ["00","10","20"],
-      ["01","11","21"],
-      ["02","12","22"],
-      ["00","11","22"],
-      ["02","11","20"],
+      ["00", "01", "02"],
+      ["10", "11", "12"],
+      ["20", "21", "22"],
+      ["00", "10", "20"],
+      ["01", "11", "21"],
+      ["02", "12", "22"],
+      ["00", "11", "22"],
+      ["02", "11", "20"],
     ]
 
-    for (let i = 0; i < tableau.length; i++) {      
-      if(document.getElementById(tableau[i][0]).innerHTML == "X" && document.getElementById(tableau[i][1]).innerHTML == "X" && document.getElementById(tableau[i][2]).innerHTML == "X"){
+    let clickedSquareContent = 0
+
+    let winner = false
+
+    for (let i = 0; i < tableau.length; i++) {
+      if (document.getElementById(tableau[i][0]).innerHTML == "X" && document.getElementById(tableau[i][1]).innerHTML == "X" && document.getElementById(tableau[i][2]).innerHTML == "X") {
+        clickedSquareContent+=1
+        winner = true
         setTimeout(() => {
           alert('Player 1 won!')
           document.location.reload(true);
-        },1000)
-
+        }, 500)
       }
-      else if(document.getElementById(tableau[i][0]).innerHTML == "O" && document.getElementById(tableau[i][1]).innerHTML == "O" && document.getElementById(tableau[i][2]).innerHTML == "O"){
+      else if (document.getElementById(tableau[i][0]).innerHTML == "O" && document.getElementById(tableau[i][1]).innerHTML == "O" && document.getElementById(tableau[i][2]).innerHTML == "O") {
+        clickedSquareContent+=1
+        winner = true
         setTimeout(() => {
           alert('Player 2 won!')
           document.location.reload(true);
-        },1000)
+        }, 500)
+      }
+      else if((document.getElementById(tableau[i][0]).innerHTML !== "X" && document.getElementById(tableau[i][1]).innerHTML !== "X" && document.getElementById(tableau[i][2]).innerHTML !== "X") && (document.getElementById(tableau[i][0]).innerHTML !== "O" && document.getElementById(tableau[i][1]).innerHTML !== "O" && document.getElementById(tableau[i][2]).innerHTML !== "O")){
+        clickedSquareContent++
+        console.log(clickedSquareContent);
+        
       }
     }
+
   }
-    
-    
-  checkedSquare()
-  {
-    // this.turn = turn
+
+  checkedSquare() {
+    let clickedSquareContent = []
+
+    let winner = false
+
     for (let i = 0; this.squares.length; i++) {
       // Display X or O depending of player's turn
-      this.squares[i].addEventListener('click', function(e) {
-        
-        if(e.target.textContent == "") {
-          if(game.turn == true){
-            
+      this.squares[i].addEventListener('click', function (e) {
+        if (e.target.textContent == "") {
+          if (game.turn == true) {
+
             e.target.textContent = "X"
-            
+
             // player1.count += 1
             // countPlayer1.textContent = player1.count
-            
+
             console.log('playerX played');
-            
+
             game.turn = !game.turn
             console.log("playerO's turn");
           }
-          else if(game.turn == false){
-            // console.log("playerO's turn");
+          else if (game.turn == false) {
+
             e.target.textContent = "O"
-            
+
             // player2.count += 1
             // countPlayer2.textContent = player2.count
-            
+
             console.log('playerO played');
             console.log("playerX's turn");
             game.turn = !game.turn
+
           }
         }
         game.win()
+
       })
     }
+    this.reset.addEventListener('click', function(e){
+      game.resetGame()
+    })
   }
 }
-            
+
+
 let game = new Game
 game.checkedSquare()
-game.win()
+game.resetGame()
 
 
 
   // Add 1s until it reach 10s
             // let countPlayer1 = document.getElementById('countPlayer1')
             // let countPlayer2 = document.getElementById('countPlayer2')
-            
-            
+
+
             // PAUSE TIMER
             //       let stop = document.getElementById('stop')
             //       stop.addEventListener('click', function(e){
               //         stopTimer()
               //         started = false
               //       })
-              
+
               //       // RESET TIMER AND GRID
               //       let reset = document.getElementById('reset')
               //       reset.addEventListener('click', function(e){
@@ -133,9 +158,9 @@ game.win()
                   //           gamestart.circle.disabled = ""
                   //       })
                   // }
-                  
-                  
-                  
+
+
+
                     //
                   // // 
 
@@ -154,7 +179,7 @@ game.win()
 //   hOnes = 0 
 //   hoursTens.textContent = hTens
 //   hoursOnes.textContent = hOnes
-  
+
 //   minutesTens.textContent = mTens
 //   minutesOnes.textContent = mOnes
 
--- src/timer.js
+++ src/timer.js
@@ -1,93 +1,99 @@
-class Timer
-{
-  
-  constructor()
-  {
-    hoursTens : document.getElementById('h-tens')
-    hoursOnes : document.getElementById('h-ones')
-    minutesTens : document.getElementById('m-tens')
-    minutesOnes : document.getElementById('m-ones')
-    secondsTens : document.getElementById('s-tens')
-    secondsOnes : document.getElementById('s-ones')
-    sTens : 0
-    sOnes : 0
-    mTens : 0
-    mOnes : 0
-    hTens : 0
-    hOnes : 0
-    this.squaresTimer = document.getElementById('test')
-  }
-     
-  startOnClick(){
-    
-        // console.log(squaresTimer[0]);
-      this.squaresTimer.addEventListener('click', function(e){
-        console.log('test');
-      })
+class Timer {
+
+    constructor() {
+        this.hoursTens = document.getElementById('h-tens')
+        this.hoursOnes = document.getElementById('h-ones')
+        this.minutesTens = document.getElementById('m-tens')
+        this.minutesOnes = document.getElementById('m-ones')
+        this.secondsTens = document.getElementById('s-tens')
+        this.secondsOnes = document.getElementById('s-ones')
+        this.sTens = 0
+        this.sOnes = 0
+        this.mTens = 0
+        this.mOnes = 0
+        this.hTens = 0
+        this.hOnes = 0
+        this.squaresTimer = document.getElementsByClassName('square')
+        this.started = false
     }
-  }
+
+    startOnClick() {
+        for (let i = 0; i < this.squaresTimer.length; i++) {
+            this.squaresTimer[i].addEventListener('click', function (e) {
+                // console.log(this.squaresTimer);
+                if(time.started == false){
+                    time.started = true
+                    setInterval(() => {
+                        time.startTimer()
+                    }, 1000);
+                }
+                else {
+                    e.preventDefault()
+                }
+            })
+        }
+    }
+
 
     startTimer() {
-    
+
         if (this.sOnes < 10) {
-          this.sOnes++
-          this.secondsOnes.textContent = this.sOnes
+            this.sOnes++
+            this.secondsOnes.textContent = this.sOnes
         }
-        
+
         // Reset seconds ones | Add 1 to seconds tens when it reach 10s
         if (this.sOnes > 9) {
-          this.sOnes = 0
-          this.secondsOnes.textContent = this.sOnes
-          this.sTens++
-          this.secondsTens.textContent = this.sTens
+            this.sOnes = 0
+            this.secondsOnes.textContent = this.sOnes
+            this.sTens++
+            this.secondsTens.textContent = this.sTens
         }
-        
+
         // Add 1 to minutes tens when it reach 10 seconds
         if (this.sOnes == 10) {
-          this.sTens++
-          this.sTens.textContent =this.sTens
+            this.sTens++
+            this.sTens.textContent = this.sTens
         }
-        
+
         // Reset seconds tens when it reach 1 minute
         if (this.sTens > 5) {
-          this.sTens = 0
-          this.secondsTens.textContent = this.sTens
-          this.mOnes++
-          this.minutesOnes.textContent = this.mOnes
+            this.sTens = 0
+            this.secondsTens.textContent = this.sTens
+            this.mOnes++
+            this.minutesOnes.textContent = this.mOnes
         }
-        
+
         // Add 1 to minutes tens when it reach 10 minutes
         if (this.mOnes > 9) {
-          this.mOnes = 0
-          this.minutesOnes.textContent = this.mOnes
-          this.mTens++
-          this.minutesTens.textContent = this.mTens
+            this.mOnes = 0
+            this.minutesOnes.textContent = this.mOnes
+            this.mTens++
+            this.minutesTens.textContent = this.mTens
         }
-        
+
         // Add 1 to hour ones | Reset minutes ones and tens when it reach 1 hour 
         if (this.mTens > 5) {
-          this.mTens = 0
-          this.minutesTens.textContent = this.mTens
-          this.mOnes = 0
-          this.minutesOnes.textContent = this.mOnes
-          this.hOnes++
-          this.hoursOnes.textContent = this.hOnes
+            this.mTens = 0
+            this.minutesTens.textContent = this.mTens
+            this.mOnes = 0
+            this.minutesOnes.textContent = this.mOnes
+            this.hOnes++
+            this.hoursOnes.textContent = this.hOnes
         }
-        
+
         // Add 1 to hour tens | Reset hour ones when it reach 10 hours
         if (this.hOnes > 9) {
-          this.hOnes = 0
-          this.hoursOnes.textContent = this.hOnes
-          this.hTens++
-          this.hoursTens.textContent = this.hTens
-          // Display message when it takes life..
-          setTimeout(() => {
-            alert('C\'est long pour un morpion...')
-          }, 1000)
+            this.hOnes = 0
+            this.hoursOnes.textContent = this.hOnes
+            this.hTens++
+            this.hoursTens.textContent = this.hTens
+            // Display message when it takes life..
+            setTimeout(() => {
+                alert('C\'est long pour un morpion...')
+            }, 1000)
         }
-  }
+    }
 }
-
 let time = new Timer
-time.startTimer()
 time.startOnClick()
