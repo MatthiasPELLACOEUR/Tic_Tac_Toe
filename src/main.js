ffc59f4662542a278644d4bf6f0202c93b636cd4
Matthias <matthias.pellacoeur@hotmail.com>
2020-06-04 12:43:34 +0200

    finished still factorised a bit more

--- src/main.js
+++ src/main.js
@@ -1,40 +1,12 @@
 class Game {
   constructor() {
-    // this.started = false
-    this.turn = true
-    // this.launchGame = document.getElementById('launchGame')
+    this.turn = true;
     // this.playerX = "X"
     // this.playerO = "O"
-    this.squares = document.getElementsByClassName('square')
-    this.reset = document.getElementById('reset')
-  }
-
-  // launchGame(){
-  // this.launchGame.addEventListener('click', function(e){
-  // When click on start button associate cross or circle on player1 and start timer
-  // if(document.getElementById('cross').checked == true){
-  //       this.assignPlayer("X", "O")
-  //     }
-  //     else if(document.getElementById('circle').checked == true){
-  //      this.assignPlayer("O", "X")
-  //      }
-
-  //    if(this.started == false){
-  //      this.started = true
-  //      timerCount = setInterval(startTimer, 1000);
-  //    }
-  //    })     
-  //  }
-  // assignPlayer(checker1, checker2){
-  //   player1 = new Player(checker1, true, 0)
-  //   player2 = new Player(checker2, false, 0)
-  // }
-
-  
-  resetGame(){
-    console.log('test');
-      // document.location.reload(true);
-      
+    this.squares = document.getElementsByClassName("square");
+    this.countPlayerO = document.getElementById("countPlayerO");
+    this.countPlayerX = document.getElementById("countPlayerX");
+    this.newGameButton = document.getElementById("newGame");
   }
 
   win() {
@@ -47,122 +19,117 @@ class Game {
       ["02", "12", "22"],
       ["00", "11", "22"],
       ["02", "11", "20"],
-    ]
-
-    let clickedSquareContent = 0
-
-    let winner = false
+    ];
 
     for (let i = 0; i < tableau.length; i++) {
-      if (document.getElementById(tableau[i][0]).innerHTML == "X" && document.getElementById(tableau[i][1]).innerHTML == "X" && document.getElementById(tableau[i][2]).innerHTML == "X") {
-        clickedSquareContent+=1
-        winner = true
+      if (
+        document.getElementById(tableau[i][0]).innerHTML == "X" &&
+        document.getElementById(tableau[i][1]).innerHTML == "X" &&
+        document.getElementById(tableau[i][2]).innerHTML == "X"
+      ) {
         setTimeout(() => {
-          alert('Player 1 won!')
+          alert("Player 1 won!");
           document.location.reload(true);
-        }, 500)
-      }
-      else if (document.getElementById(tableau[i][0]).innerHTML == "O" && document.getElementById(tableau[i][1]).innerHTML == "O" && document.getElementById(tableau[i][2]).innerHTML == "O") {
-        clickedSquareContent+=1
-        winner = true
-        setTimeout(() => {
-          alert('Player 2 won!')
-          document.location.reload(true);
-        }, 500)
-      }
-      else if((document.getElementById(tableau[i][0]).innerHTML !== "X" && document.getElementById(tableau[i][1]).innerHTML !== "X" && document.getElementById(tableau[i][2]).innerHTML !== "X") && (document.getElementById(tableau[i][0]).innerHTML !== "O" && document.getElementById(tableau[i][1]).innerHTML !== "O" && document.getElementById(tableau[i][2]).innerHTML !== "O")){
-        clickedSquareContent++
-        console.log(clickedSquareContent);
-        
+        }, 500);
+      } else if (
+        document.getElementById(tableau[i][0]).innerHTML == "O" &&
+        document.getElementById(tableau[i][1]).innerHTML == "O" &&
+        document.getElementById(tableau[i][2]).innerHTML == "O"
+        ) {
+          setTimeout(() => {
+            alert("Player 2 won!");
+            document.location.reload(true);
+          }, 500);
+        }
       }
     }
-
-  }
-
-  checkedSquare() {
-    let clickedSquareContent = []
-
-    let winner = false
-
-    for (let i = 0; this.squares.length; i++) {
-      // Display X or O depending of player's turn
-      this.squares[i].addEventListener('click', function (e) {
-        if (e.target.textContent == "") {
-          if (game.turn == true) {
-
-            e.target.textContent = "X"
-
-            // player1.count += 1
-            // countPlayer1.textContent = player1.count
-
-            console.log('playerX played');
-
-            game.turn = !game.turn
-            console.log("playerO's turn");
-          }
-          else if (game.turn == false) {
-
-            e.target.textContent = "O"
-
-            // player2.count += 1
-            // countPlayer2.textContent = player2.count
-
-            console.log('playerO played');
-            console.log("playerX's turn");
-            game.turn = !game.turn
-
-          }
-        }
-        game.win()
-
+    
+    newGame() {
+  
+      this.newGameButton.addEventListener('click', (e) =>{
+         document.location.reload(true);
       })
     }
-    this.reset.addEventListener('click', function(e){
-      game.resetGame()
-    })
+  
+  checkedSquare() {
+    for (let i = 0; this.squares.length; i++) {
+      // window.onload = function () {
+        this.squares[i].addEventListener("click", function (e) {
+          if (e.target.textContent == "") {
+            if (game.turn == true) {
+              let countX = parseInt(game.countPlayerX.innerHTML);
+
+              e.target.textContent = "X";
+
+              countX += 1;
+              
+              let countXString = countX.toString();
+              
+              game.countPlayerX.innerHTML = countXString;
+              
+              console.log("playerX played");
+
+              game.turn = !game.turn;
+              console.log("playerO's turn");
+            } else if (game.turn == false) {
+              let countO = parseInt(game.countPlayerO.innerHTML);
+              
+              e.target.textContent = "O";
+              
+              countO += 1;
+              
+              let countOString = countO.toString();
+
+              game.countPlayerO.innerHTML = countOString;
+              
+              // player2.count += 1
+              // countPlayer2.textContent = player2.count
+              
+              console.log("playerO played");
+              console.log("playerX's turn");
+              game.turn = !game.turn;
+            }
+          }
+          game.win();
+        });
+        game.newGame();
+        // };
+      }
+    }
   }
-}
+  
+  console.log("hey matthias");
+let game = new Game();
+console.log(game);
+game.checkedSquare();
+// Add 1s until it reach 10s
 
+// PAUSE TIMER
+//       let stop = document.getElementById('stop')
+//       stop.addEventListener('click', function(e){
+//         stopTimer()
+//         started = false
+//       })
 
-let game = new Game
-game.checkedSquare()
-game.resetGame()
+//       // RESET TIMER AND GRID
+//       let reset = document.getElementById('reset')
+//       reset.addEventListener('click', function(e){
+//           if(squares[i].textContent !== ""){
+//               squares[i].textContent = ""
+//           }
+//           countPlayer1.textContent = "0"
+//           player1.count = 0
+//           countPlayer2.textContent = "0"
+//           player2.count = 0
+//           player1.turn = true
+//           resetTimer()
+//           gamestart.cross.disabled = ""
+//           gamestart.circle.disabled = ""
+//       })
+// }
 
-
-
-  // Add 1s until it reach 10s
-            // let countPlayer1 = document.getElementById('countPlayer1')
-            // let countPlayer2 = document.getElementById('countPlayer2')
-
-
-            // PAUSE TIMER
-            //       let stop = document.getElementById('stop')
-            //       stop.addEventListener('click', function(e){
-              //         stopTimer()
-              //         started = false
-              //       })
-
-              //       // RESET TIMER AND GRID
-              //       let reset = document.getElementById('reset')
-              //       reset.addEventListener('click', function(e){
-                //           if(squares[i].textContent !== ""){
-                  //               squares[i].textContent = ""
-                  //           }
-                  //           countPlayer1.textContent = "0"
-                  //           player1.count = 0
-                  //           countPlayer2.textContent = "0"
-                  //           player2.count = 0
-                  //           player1.turn = true
-                  //           resetTimer()
-                  //           gamestart.cross.disabled = ""
-                  //           gamestart.circle.disabled = ""
-                  //       })
-                  // }
-
-
-
-                    //
-                  // // 
+//
+// //
 
 // //STOP TIMER
 // function stopTimer(){
@@ -176,7 +143,7 @@ game.resetGame()
 //   mTens = 0
 //   mOnes = 0
 //   hTens = 0
-//   hOnes = 0 
+//   hOnes = 0
 //   hoursTens.textContent = hTens
 //   hoursOnes.textContent = hOnes
 
